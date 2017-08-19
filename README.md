68b1318bc95ad5d7c7e92c031d4fe2774dfdebfe
Matthew "Juniper" Barlett <emeraldinspirations+asusrog+1610@gmail.com>
2017-08-19 02:21:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+![emeraldinspirations logo](http://vps56132.vps.ovh.ca/logo.gitHub.png)
+
+# tool-autodeploy
+> An tool by [emeraldinspiration](https://github.com/emeraldinspirations)'s
+
+Auto deployment script for demo site
+
+## Installing / Getting started
+
+This project has no dependencies and can be cloned directly from the git repo
+
+### Clone with HTTPS
+
+```shell
+git clone https://github.com/emeraldinspirations/tool-autodeploy.git
+```
+
+### Clone with SSH
+
+```shell
+git clone git@github.com:emeraldinspirations/tool-autodeploy.git
+```
+
+## Contributing
+
+If you'd like to contribute, please fork the repository and use a feature branch.
+
+I am also open to feedback about how well I am being compliant with standards and "best practices." I have written software solo for years, and am trying to learn how to work better with others.
+
+## Licensing
+
+The code in this project is licensed under [MIT license](LICENSE).
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Container for Auto deployment script for demo site
+ *
+ * PHP Version 7
+ *
+ * @category  Tool
+ * @package   DemoAutoDeploy
+ * @author    Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @copyright 2017 Matthew "Juniper" Barlett <emeraldinspirations@gmail.com>
+ * @license   MIT LICENSE
+ * @version   GIT: $Id$
+ * @link      https://github.com/emeraldinspirations/tool-autodeploy
+ *
+ * @link https://gist.github.com/oodavid/1809044
+ * @link http://isometriks.com/verify-github-webhooks-with-php
+ */
+
+// Sets $Secret
+require 'secret.local.php';
+
+$Headers = getallheaders();
+$HubSignature = $Headers['X-Hub-Signature'];
+
+// Split signature into algorithm and hash
+list($Algo, $Hash) = explode('=', $HubSignature, 2);
+
+// Get payload
+$Payload = file_get_contents('php://input');
+
+// Calculate hash based on payload and the secret
+$PayloadHash = hash_hmac($Algo, $Payload, $Secret);
+
+// Check if hashes are equivalent
+if ($Hash !== $PayloadHash) {
+    // Kill the script or do something else here.
+    die('Bad secret');
+}
+
+$DemoDirectory = dir('..');
+
+while (false !== ($Entry = $DemoDirectory->read())) {
+
+    if (in_array(
+        $Entry,
+        [
+            '.',
+            '..',
+        ]
+    )
+    ) {
+        continue;
+    }
+
+    exec(
+        'git -C '
+        . $DemoDirectory->path
+        . DIRECTORY_SEPARATOR
+        . $Entry
+        . ' pull --verify-signatures'
+    );
+
+}
